84484c49ff23f15a4e37f7a68c3cb52dd7305c19
Yingxin Zhang <s3752708@student.rmit.edu.au>
2023-05-25 17:40:02 +1000

    add the api function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import CreateTask from "./components/CreateTask";
 import TaskList from "./components/TaskList";
 import { Box } from "@mui/material";
@@ -15,6 +16,49 @@ const TaskForm = styled(Box)`
 function App() {
   const [tasks, setTasks] = useState([]);
   const [filter, setFilter] = useState("all");
+
+  const fetchTask = async () => {
+    const response = await axios.get("http://localhost:3001/tasks");
+    setTasks(response.data);
+  };
+
+  useEffect(() => {
+    fetchTask();
+  }, []);
+
+  const createTask = async (title) => {
+    const response = await axios.post("http://localhost:3001/tasks", {
+      title,
+      status: "inProgress",
+    });
+    const updatedTasks = [...tasks, response.data];
+    setTasks(updatedTasks);
+  };
+
+  const deleteTasksById = async (id) => {
+    await axios.delete(`http://localhost:3001/tasks/${id}`);
+    const updatedTasks = tasks.filter((task) => {
+      return task.id !== id;
+    });
+    setTasks(updatedTasks);
+  };
+
+  const editTaskById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/tasks/${id}`, {
+      title: newTitle,
+    });
+
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return {
+          ...task,
+          ...response.data,
+        };
+      }
+      return tasks;
+    });
+    setTasks(updatedTasks);
+  };
 
   const handleFilterTasks = (filter) => {
     setFilter(filter);
@@ -37,37 +81,6 @@ function App() {
         return {
           ...task,
           status: nextStatus,
-        };
-      }
-      return task;
-    });
-    setTasks(updatedTasks);
-  };
-
-  const createTask = (title) => {
-    const updatedTasks = [
-      ...tasks,
-      {
-        id: Math.random().toString(),
-        title,
-        status: "inProgress",
-      },
-    ];
-    setTasks(updatedTasks);
-  };
-  const deleteTasksById = (id) => {
-    const updatedTasks = tasks.filter((task) => {
-      return task.id !== id;
-    });
-    setTasks(updatedTasks);
-  };
-
-  const editTaskById = (id, newTitle) => {
-    const updatedTasks = tasks.map((task) => {
-      if (task.id === id) {
-        return {
-          ...task,
-          title: newTitle,
         };
       }
       return task;
