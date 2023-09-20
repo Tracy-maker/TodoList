e4f9f5785e0899b990e810d704e7117f0316800f
Tracy-maker <ydlvns@gmail.com>
2023-09-20 13:21:47 +1000

    1

---
+++ .history/src/components/CreateTask_20230920121622.js
@@ -0,0 +1,56 @@
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { useState } from "react";
+import { styled } from "@mui/material";
+
+const Input = styled(TextField)`
+  padding: 5%;
+  width: 65%;
+`;
+
+const AddButton = styled(Button)`
+  margin: auto;
+  width: 20%;
+  padding: 10px;
+  text-align: center;
+`;
+
+function CreateTask(props) {
+  const [title, setTitle] = useState("");
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (title.trim() !== "") {
+      props.onCreate(title);
+      setTitle("");
+    }
+  };
+
+  return (
+    <Stack onSubmit={handleSubmit} component="form" direction="row">
+      <Input
+        value={title}
+        onChange={handleChange}
+        placeholder="Add a title"
+        variant="filled"
+      />
+      <Input
+        value={title}
+        onChange={handleChange}
+        placeholder="Add description"
+        variant="filled"
+      />
+      <AddButton type="submit" variant="contained">
+        Add Task
+      </AddButton>
+    </Stack>
+  );
+}
+
+export default CreateTask;
